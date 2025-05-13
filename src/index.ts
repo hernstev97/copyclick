965fd2ee2f997eb341a818de6968a05d618d2620
hernstev97 <hernandezsteven1997@gmail.com>
2025-05-13 21:54:30 +0200

    Add baseline html and css

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("test")
+const changeColorMode = (isDark: Boolean) => {
+    if (isDark)
+        document.body.classList.add('darkmode');
+    else
+        document.body.classList.remove('darkmode');
+}
+
+const mq = window.matchMedia('(prefers-color-scheme: dark)');
+const isDark = mq.matches;
+changeColorMode(isDark);
+
+mq.addEventListener('change', function (evt) {
+    const isDark = evt.matches;
+    changeColorMode(isDark);
+});
